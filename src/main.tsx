8bd8e112e4d78ddb8558b0a6fcd8755557857200
jm-316 <yoyo3302@gmail.com>
2024-05-16 15:46:35 +0900

    set: 초기설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import App from './App.tsx';
+import Home from './pages/home/index.tsx';
+import PostListPage from './pages/posts/index.tsx';
+import PostDetailPage from './pages/posts/detail.tsx';
+import PostEditPage from './pages/posts/edit.tsx';
+import PostNewPage from './pages/posts/new.tsx';
+import NotificationsPage from './pages/notifications/index.tsx';
+import ProfilePage from './pages/profile/index.tsx';
+import ProfileEditPage from './pages/profile/edit.tsx';
+import SearchPage from './pages/search/index.tsx';
+import LoginPage from './pages/users/login.tsx';
+import SignupPage from './pages/users/signup.tsx';
+import NotFoundPage from './pages/notFound/index.tsx';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFoundPage />,
+    children: [{ index: true, element: <Home /> }],
+  },
+  {
+    path: 'posts',
+    children: [
+      { path: '/posts', element: <PostListPage /> },
+      { path: '/posts/:id', element: <PostDetailPage /> },
+      { path: '/posts/edit/:id', element: <PostEditPage /> },
+      { path: '/posts/new', element: <PostNewPage /> },
+    ],
+  },
+  {
+    path: 'users',
+    children: [
+      { path: 'login', element: <LoginPage /> },
+      { path: 'signup', element: <SignupPage /> },
+    ],
+  },
+  { path: '/profile', element: <ProfilePage /> },
+  { path: '/profile/edit', element: <ProfileEditPage /> },
+  { path: '/notifications', element: <NotificationsPage /> },
+  { path: '/search', element: <SearchPage /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
