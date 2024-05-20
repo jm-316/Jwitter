f0132340a4417aec42770b84a15183cb21680693
jm-316 <yoyo3302@gmail.com>
2024-05-20 20:59:42 +0900

    PostProps 타입 추가

--- src/type/index.ts
+++ src/type/index.ts
@@ -3,3 +3,16 @@ import { ReactNode } from 'react';
 export interface AuthProps {
   children: ReactNode;
 }
+
+export interface PostProps {
+  id: string;
+  email: string;
+  content: string;
+  createdAt: string;
+  uid: string;
+  profileUrl?: string;
+  hashTags?: string[];
+  imageUrl?: string;
+  likes?: string[];
+  likeCount?: number;
+}
