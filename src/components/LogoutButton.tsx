4989e6dd0aa322a2f113b1844e8686a788d4819c
jm-316 <yoyo3302@gmail.com>
2024-05-20 11:11:19 +0900

    로그아웃 기능 구현

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,14 +1,30 @@
+import { FaUserCircle } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+import { useContext } from 'react';
+import { logOut } from '../api/firebase';
+import AuthContext from './context/AuthContext';
 import styles from './LogoutButton.module.scss';
 
 export default function LogoutButton() {
+  const { user } = useContext(AuthContext);
+
+  const handleLogout = async () => {
+    await logOut();
+    toast.success('로그아웃 되었습니다.');
+  };
+
   return (
-    <button className={styles.logOut__btn}>
+    <button className={styles.logOut__btn} type="button" onClick={handleLogout}>
       <div className={styles.logOut__btn__image}>
-        <img src="./vite.svg" alt="profile" />
+        {user?.photoURL ? (
+          <img src={user?.photoURL} alt="profile" />
+        ) : (
+          <FaUserCircle className={styles.logOut__btn__imageIcon} />
+        )}
       </div>
       <div className={styles.logOut__btn__username}>
-        <div>name</div>
-        <div>email</div>
+        <div>{user?.displayName ?? 'user'}</div>
+        <div>{user?.email ?? '@' + user?.uid?.slice(0, 5)}</div>
       </div>
     </button>
   );
