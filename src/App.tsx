1891a423ee5e2d77c2b54af3abba5535a3491fcb
jm-316 <yoyo3302@gmail.com>
2024-05-20 11:10:46 +0900

    toastify 위치 변경 및 로딩 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import { ToastContainer } from 'react-toastify';
 import AppLayout from './components/layout/AppLayout';
-import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
-      <ToastContainer
-        theme="dark"
-        autoClose={1000}
-        hideProgressBar
-        newestOnTop
-      />
       <AppLayout />
     </>
   );
--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,28 +1,51 @@
 import { Outlet } from 'react-router';
 import { Link } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { useContext } from 'react';
+import AuthContext from '../context/AuthContext';
 import Menu from '../Menu';
 import LogoutButton from '../LogoutButton';
+import Loader from '../loader/Loader';
+import 'react-toastify/dist/ReactToastify.css';
 import styles from './AppLayout.module.scss';
 
 export default function AppLayout() {
+  const { user, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
-    <div className={styles.container}>
-      <header className={styles.left__section__wrapper}>
-        <section className={styles.left__section}>
-          <div className={styles.left__sectionFixed}>
-            <Link to="/" className={styles.logo}>
-              <div className={styles.logoPill}>
-                <img src="./logo.png" alt="logo" width={60} height={60} />
+    <>
+      <ToastContainer
+        theme="dark"
+        autoClose={1000}
+        hideProgressBar
+        newestOnTop
+      />
+      {user ? (
+        <div className={styles.container}>
+          <header className={styles.left__section__wrapper}>
+            <section className={styles.left__section}>
+              <div className={styles.left__sectionFixed}>
+                <Link to="/" className={styles.logo}>
+                  <div className={styles.logoPill}>
+                    <img src="./logo.png" alt="logo" width={60} height={60} />
+                  </div>
+                </Link>
               </div>
-            </Link>
+            </section>
+            <Menu />
+            <LogoutButton />
+          </header>
+          <div className={styles.right__section}>
+            <Outlet />
           </div>
-        </section>
-        <Menu />
-        <LogoutButton />
-      </header>
-      <div className={styles.right__section}>
+        </div>
+      ) : (
         <Outlet />
-      </div>
-    </div>
+      )}
+    </>
   );
 }
