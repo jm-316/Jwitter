30d20d7c895456ed98afa1722d984260c1149d75
jm-316 <yoyo3302@gmail.com>
2024-05-21 16:19:11 +0900

    postDeail page 구현

---
+++ src/components/posts/PostDetail.tsx
@@ -0,0 +1,37 @@
+import { useParams } from 'react-router';
+import { IoSearchSharp } from 'react-icons/io5';
+import { useEffect, useState } from 'react';
+import { getPost } from '../../api/firebase';
+import PostList from './PostList';
+import Loader from '../loader/Loader';
+import Header from '../Header';
+import { PostProps } from '../../type';
+import styles from './PostDetail.module.scss';
+
+export default function PostDetail() {
+  const [post, setPost] = useState<PostProps | null>(null);
+
+  const params = useParams();
+
+  useEffect(() => {
+    if (params.id) {
+      getPost(params.id, setPost);
+    }
+  }, [params.id]);
+
+  return (
+    <>
+      <div className={styles.home}>
+        <Header />
+        {post ? <PostList post={post} /> : <Loader />}
+      </div>
+      <div className={styles.search}>
+        <div className={styles.search__div}>
+          <IoSearchSharp className={styles.search__icon} />
+          <input placeholder="해시태그 검색" className={styles.search__input} />
+        </div>
+        <div className={styles.search__post}>검색 내역이 없습니다.</div>
+      </div>
+    </>
+  );
+}
