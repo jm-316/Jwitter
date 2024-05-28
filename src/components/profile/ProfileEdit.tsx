c427a3e7823ab2a7e31dc0304d578943e372741b
jm-316 <yoyo3302@gmail.com>
2024-05-28 15:10:15 +0900

    profile edit page 구현

--- src/components/profile/ProfileEdit.tsx
+++ src/components/profile/ProfileEdit.tsx
@@ -1,26 +1,103 @@
+import { useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+import { FaUserCircle } from 'react-icons/fa';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from '../Header';
 import Search from '../search/Search';
+import AuthContext from '../context/AuthContext';
+import { deleteImage, updatedProfile, uploadImage } from '../../api/firebase';
+import { getErrorMessage } from '../../util/error';
 import styles from './ProfileEdit.module.scss';
 
 export default function ProfileEdit() {
+  const [displayName, setDisplayName] = useState<string>('');
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = e;
+
+    setDisplayName(value);
+  };
+
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { files },
+    } = e;
+
+    const file = files?.[0];
+    if (file) {
+      const fileReader = new FileReader();
+      fileReader?.readAsDataURL(file);
+
+      fileReader.onloadend = (e: ProgressEvent<FileReader>) => {
+        const { result } = e?.currentTarget as FileReader;
+        setImageUrl(result as string);
+      };
+    }
+  };
+  useEffect(() => {
+    if (user?.photoURL) {
+      setImageUrl(user?.photoURL);
+    }
+
+    if (user?.displayName) {
+      setDisplayName(user?.displayName);
+    }
+  }, [user?.photoURL, user?.displayName]);
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      let newImageUrl = '';
+      if (user?.photoURL && imageUrl && imageUrl !== user.photoURL) {
+        await deleteImage(user.photoURL);
+        newImageUrl = await uploadImage(user?.uid, imageUrl);
+      }
+      if (user) {
+        await updatedProfile(user, displayName, newImageUrl);
+      }
+
+      toast.success('프로필이 업데이트 되었습니다.');
+      navigate('/profile');
+    } catch (error) {
+      const errorMessage = getErrorMessage(error);
+      toast.error(errorMessage);
+    }
+  };
+
   return (
     <>
       <div className={styles.home}>
         <div className={styles.profile__editContainer}>
           <Header />
-          <form className={styles.editForm}>
+          <form className={styles.editForm} onSubmit={onSubmit}>
             <div className={styles.editForm__flex}>
               <div className={styles.editForm__imgBox}>
-                <img src="/logo.png" className={styles.editForm__img} />
-                <label htmlFor="test" className={styles.editForm__img__label}>
+                {imageUrl ? (
+                  <img src={imageUrl} className={styles.editForm__img} />
+                ) : (
+                  <FaUserCircle className={styles.editForm__imgIcon} />
+                )}
+
+                <label
+                  htmlFor="file-input"
+                  className={styles.editForm__img__label}
+                >
                   변경하기
                 </label>
                 <input
                   type="file"
-                  name="file-input"
                   accept="image/*"
-                  id="test"
+                  name="file-input"
+                  id="file-input"
                   className={styles.hidden}
+                  onChange={handleFileUpload}
                 />
               </div>
               <div className={styles.editForm__profile}>
@@ -28,13 +105,19 @@ export default function ProfileEdit() {
                   type="text"
                   name="displayName"
                   className={styles.editForm__profile__input}
-                  placeholder="이름"
+                  placeholder="user"
+                  onChange={onChange}
+                  value={displayName}
                 />
-                <div className={styles.editForm__profile__email}>emil</div>
+                <div className={styles.editForm__profile__email}>
+                  {user?.email}
+                </div>
               </div>
             </div>
             <div className={styles.editForm__submitArea}>
-              <button className={styles.editForm__btn}>수정</button>
+              <button className={styles.editForm__btn} type="submit">
+                수정
+              </button>
             </div>
           </form>
         </div>
